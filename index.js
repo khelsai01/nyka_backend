a90258d069a93244b768bb3203c5120f18628798
Khelsai Rajwade <khelsai@gmail.com>
2024-01-05 11:50:36 +0530

    correction in port

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-require("dotenv").config();
 const { connection } = require("./connection/dbconnection");
 const { userRouter } = require("./routes/user.routes");
 const { productRouter } = require("./routes/product.routes");
@@ -9,14 +8,14 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(errorHandler)
-app.use("/api",userRouter,productRouter)
+app.use(errorHandler);
+app.use("/api",userRouter,productRouter);
 
-app.listen(process.env.PORT, async()=>{
+app.listen(8000, async()=>{
     try {
         await connection;
         console.log('server is connected to database')
-        console.log(`server is running at port ${process.env.PORT}`)
+        console.log(`server is running at port 8000`)
     } catch (error) {
         console.log(error)
     }
